543b1784d5567ca01ebb56bceb090186dc5d83a9
gdfgghghfhfg <156810499+sssssigma420@users.noreply.github.com>
2025-09-18 19:52:01 +0300

    Update lib.lua

--- lib.lua
+++ lib.lua
@@ -1179,7 +1179,7 @@ local function createSection(page, title)
     Section.Name = "Section"
     Section.Size = UDim2.new(1, 0, 0, 0)
     Section.AutomaticSize = Enum.AutomaticSize.Y
-    GlassBlur = Color3.fromRGB(30, 30, 35),
+    GlassBlur = Color3.fromRGB(30, 30, 35)
     Section.BackgroundTransparency = 0
     Section.BorderSizePixel = 0
     Section.Parent = page
