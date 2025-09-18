86514fdf3c68f22d7b0665a8616db6d8550addbe
gdfgghghfhfg <156810499+sssssigma420@users.noreply.github.com>
2025-09-18 12:57:25 +0300

    Update lib.lua

--- lib.lua
+++ lib.lua
@@ -655,8 +655,13 @@ local IndicatorBorder = Instance.new("UIStroke", OnlineIndicator)
 IndicatorBorder.Color = Theme.Background
 IndicatorBorder.Thickness = 2
 
-ContentArea.BackgroundTransparency = 0
-ContentArea.BackgroundColor3 = Theme.Background  -- or Color3.fromRGB(15, 15, 17)
+
+local ContentArea = Instance.new("Frame")
+ContentArea.Name = "ContentArea"
+ContentArea.Size = UDim2.new(1, -240, 1, -50)
+ContentArea.Position = UDim2.new(0, 240, 0, 50)
+ContentArea.BackgroundTransparency = 0  -- Change this line
+ContentArea.Parent = MainWindow
 
 -- Pages container
 local PagesContainer = Instance.new("Frame")
